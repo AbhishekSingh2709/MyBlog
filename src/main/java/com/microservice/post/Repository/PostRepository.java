8064dfd8ea9d7e0f7bc88255478638220a906129
Abhishek Singh <singhabhishek696932@gmail.com>
2024-07-26 23:03:08 +0530

    Delete src directory

--- src/main/java/com/microservice/post/Repository/PostRepository.java
+++
@@ -1,7 +0,0 @@
-package com.microservice.post.Repository;
-
-import com.microservice.post.Entity.Post;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PostRepository extends JpaRepository<Post, String> {
-}
